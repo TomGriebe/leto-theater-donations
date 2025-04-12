926a81a89b289dfc8acfc1f344c932fd2b5db60a
Tom Griebe <tom.griebe@gmail.com>
2025-04-12 12:49:27 +0200

    save streamlabs api key

--- src/leto_theater_donations.py
+++ src/leto_theater_donations.py
@@ -6,15 +6,17 @@ import animations
 import donations
 import obs_logging
 import sources
+import streamlabs
 
 importlib.reload(animations)
 importlib.reload(donations)
 importlib.reload(obs_logging)
 importlib.reload(sources)
+importlib.reload(streamlabs)
 
 
 def try_setup():
-    log_info("Loading script...")
+    log_info("Trying to set up sources")
     idle_source = sources.get_idle_source()
 
     if idle_source:
@@ -47,15 +49,26 @@ def try_setup():
 
 # This is run as soon as the script is loaded, to set up the basic event handling.
 def script_load(_):
-    obs.timer_add(try_setup, 1000)
+    log_info("Loading script")
+    obs.timer_add(try_setup, 500)
+
+
+def script_update(settings):
+    streamlabs.api_key = obs.obs_data_get_string(settings, "streamlabs_api_key")
 
 
 # This function sets up the UI properties (like buttons) for the script.
 def script_properties():
     props = obs.obs_properties_create()
+
+    # Streamlabs API Key
+    obs.obs_properties_add_text(props, "streamlabs_api_key", "Streamlabs API Key", obs.OBS_TEXT_PASSWORD)
+
+    # Mock donation buttons
     obs.obs_properties_add_button(props, "donate_1_btn", "Donate $1!", donations.handle_donation_1_usd)
     obs.obs_properties_add_button(props, "donate_5_btn", "Donate $5!", donations.handle_donation_5_usd)
     obs.obs_properties_add_button(props, "donate_10_btn", "Donate $10!", donations.handle_donation_10_usd)
+
     return props
 
 
